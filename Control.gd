d96f887aba9e2e4bf78712961472c261ba4f4b96
Keroflux <49621236+Keroflux@users.noreply.github.com>
2019-12-03 00:09:30 +0100

    Optimized and simplified logic
    Simplified is_tag function. Removed a lot of unnecessary code. Added version number in top left window

--- Control.gd
+++ Control.gd
@@ -1,14 +1,5 @@
 extends Control
 
-var words =   0
-var charcnt = 0
-
-var lq =  0
-var p1 =  0
-var dp =  0
-var rp =  0
-var tot = 0
-
 var TAG =  []   # Liste for alle tagnummer
 var TAGL = []   # Liste for LQ tagnummer
 var TAGA = []   # Liste for P1 tagnummer
@@ -24,8 +15,9 @@ func _ready():
 # Søker gjennom teksten fra utklippstaveln. Deler den opp i ord som sendes
 # ett og ett til is_tag funksjonen
 func search():
-	words = 0
-	charcnt = 0
+	
+	var words = 0
+	var charcnt = 0
 	var message = OS.get_clipboard()
 	var textList = message.split('\n')
 	var jointList = textList.join(' ')
@@ -35,16 +27,28 @@ func search():
 		words += 1
 		charcnt += len(textList[i])
 		is_tag(textList[i])
+	# Teller antall tagnummer i listene
+	var lq = len(TAGL)
+	var p1 = len(TAGA)
+	var dp = len(TAGD)
+	var rp = len(TAGR)
+	var tot = lq + p1 + dp + rp
 	
-	#p1 = len(TAGA) # Test av ny telle funksjon!
-#	if not $Multifile.pressed:
-#		TAG = TAGL + TAGA + TAGD + TAGR
+	if not $Multifile.pressed: # Lagrer tag i felles liste
+		TAG = TAGL + TAGA + TAGD + TAGR
+		TAGL = []
+		TAGA = []
+		TAGD = []
+		TAGR = []
+	
 	$Label.text = 'Søkte gjennom ' + str(words) + ' ord og ' + str(charcnt) + ' tegn'
 	$TAG.text = 'Fant følgende TAG: \n LQ: ' + str(lq) + '\n P1: ' + str(p1) + '\n DP: ' + str(dp) + '\n RP: ' + str(rp) + '\n Total: ' + str(tot)
 
 
-# Funksjon som søker etter tag nummer, ord for ord og plasserer dem i lister
+# Får ett og ett ord fra search funksjonen og ser om det matcher tag formatet til JSF
+# Alle matcher blir puttet i lister
 func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner, kanskje.
+	
 	if len(text) < 9:
 		return false
 	for x in range(0, 3):
@@ -56,68 +60,33 @@ func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner
 		return false
 	if text[6] != '-':
 		return false
-	if $Prefix.pressed:
-		text = text.lstrip('123456790-.')
-		if $Multifile.pressed:
-			if text.begins_with('L') and not TAGL.has(text):
-				TAGL.append(text)
-				lq += 1
-			if text.begins_with('A') and not TAGA.has(text):
-				TAGA.append(text)
-				p1 += 1
-			if text.begins_with('D') and not TAGD.has(text):
-				TAGD.append(text)
-				dp += 1
-			if text.begins_with('R') and not TAGR.has(text):
-				TAGR.append(text)
-				rp += 1
-		else:
-			if text.begins_with('L') and not TAG.has(text):
-				TAG.append(text)
-				lq += 1
-			if text.begins_with('A') and not TAG.has(text):
-				TAG.append(text)
-				p1 += 1
-			if text.begins_with('D') and not TAG.has(text):
-				TAG.append(text)
-				dp += 1
-			if text.begins_with('R') and not TAG.has(text):
-				TAG.append(text)
-				rp += 1
-	else:
-		if $Multifile.pressed:
-			if text[5] == 'L' and not TAGL.has(text):
-				TAGL.append(text)
-				lq += 1
-			if text[5] == 'A' and not TAGA.has(text):
-				TAGA.append(text)
-				p1 += 1
-			if text[5] == 'D' and not TAGD.has(text):
-				TAGD.append(text)
-				dp += 1
-			if text[5] == 'R' and not TAGR.has(text):
-				TAGR.append(text)
-				rp += 1
-		else:
-			if text[5] == 'L' and not TAG.has(text):
-				TAG.append(text)
-				lq += 1
-			if text[5] == 'A' and not TAG.has(text):
-				TAG.append(text)
-				p1 += 1
-			if text[5] == 'D' and not TAG.has(text):
-				TAG.append(text)
-				dp += 1
-			if text[5] == 'R' and not TAG.has(text):
-				TAG.append(text)
-				rp += 1
 	
-	tot = lq + p1 + dp + rp
+	if $Prefix.pressed: 
+		text = text.lstrip('123456790-.') # Fjernet 1900 prefix
+		if text.begins_with('L') and not TAGL.has(text):
+			TAGL.append(text)
+		if text.begins_with('A') and not TAGA.has(text):
+			TAGA.append(text)
+		if text.begins_with('D') and not TAGD.has(text):
+			TAGD.append(text)
+		if text.begins_with('R') and not TAGR.has(text):
+			TAGR.append(text)
+	else:
+		if text[5] == 'L' and not TAGL.has(text):
+			TAGL.append(text)
+		if text[5] == 'A' and not TAGA.has(text):
+			TAGA.append(text)
+		if text[5] == 'D' and not TAGD.has(text):
+			TAGD.append(text)
+		if text[5] == 'R' and not TAGR.has(text):
+			TAGR.append(text)
+
 	return true # TODO: Return text i stedet?
 
 
 # Funksjon for å lagre tag i .txt filer
 func save(text, id):
+	
 	if len(text) > 0:
 		var tag = PoolStringArray(text)
 		var tagsort = tag.join('\n')
@@ -129,28 +98,28 @@ func save(text, id):
 
 # Knapp for generering av lister
 func _on_GenererLister_pressed():
+	
 	save(TAGL, 'JSL')
 	save(TAGA, 'JSA')
 	save(TAGD, 'JSD')
 	save(TAGR, 'JSR')
 	save(TAG, 'JSF')
+	
 	$Label.text = 'Lister opprettet i C:\\APPL'
-	if $Taghub.pressed:
+	
+	if $Taghub.pressed: # Åpner TagHub
 		OS.shell_open('https://taghub.equinor.com/')
 
 
 # Knapp for last/søk på nytt
 func _on_Reload_pressed():
+	
 	TAGL = []
 	TAGA = []
 	TAGD = []
 	TAGR = []
 	TAG = []
-	lq = 0
-	p1 = 0
-	dp = 0
-	rp = 0
-	tot = 0
+	
 	search()
 
 
