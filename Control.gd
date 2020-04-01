c37abec99ca6df06ff804fbff48f572f83f808b0
Keroflux <brekkis90@gmail.com>
2020-04-01 10:32:33 +0200

    Updated UI
    Better UI with an added scrollbox that displays all the found tags

--- Control.gd
+++ Control.gd
@@ -1,24 +1,26 @@
 extends Control
 
+var dir = OS.get_system_dir(2) # Får adressen til dokumenter. Brukes til lagring
+
 var TAG =  []   # Liste for alle tagnummer
 var TAGL = []   # Liste for LQ tagnummer
 var TAGA = []   # Liste for P1 tagnummer
 var TAGD = []   # Liste for DP tagnummer
 var TAGR = []   # Liste for RP tagnummer
 
+var TAGlabel = preload("res://TAG.tscn") # Label som viser funnede tag i scrollboxen
 
-# Kjøres ved oppstart av programmet
+
 func _ready():
-	search()
+	search() # Kjører et søk ved oppstart av programmet
 
 
 # Søker gjennom teksten fra utklippstaveln. Deler den opp i ord som sendes
 # ett og ett til is_tag funksjonen
 func search():
-	
 	var words = 0
 	var charcnt = 0
-	var message = OS.get_clipboard()
+	var message = OS.get_clipboard() # Får teksten fra clipboardet til PCen
 	var textList = message.split('\n')
 	var jointList = textList.join(' ')
 	textList = jointList.split(' ')
@@ -34,21 +36,14 @@ func search():
 	var rp = len(TAGR)
 	var tot = lq + p1 + dp + rp
 	
-	if not $Multifile.pressed: # Lagrer tag i felles liste
-		TAG = TAGL + TAGA + TAGD + TAGR
-		TAGL = []
-		TAGA = []
-		TAGD = []
-		TAGR = []
+	$MC/VBC/Label.text = 'Søkte gjennom ' + str(words) + ' ord og ' + str(charcnt) + ' tegn'
+	$MC/VBC/TAG.text = 'Fant følgende TAG: LQ: ' + str(lq) + ' P1: ' + str(p1) + ' DP: ' + str(dp) + ' RP: ' + str(rp) + ' Total: ' + str(tot)
+	fill_scrollbox()
 	
-	$Label.text = 'Søkte gjennom ' + str(words) + ' ord og ' + str(charcnt) + ' tegn'
-	$TAG.text = 'Fant følgende TAG: \n LQ: ' + str(lq) + '\n P1: ' + str(p1) + '\n DP: ' + str(dp) + '\n RP: ' + str(rp) + '\n Total: ' + str(tot)
-
 
 # Får ett og ett ord fra search funksjonen og ser om det matcher tag formatet til JSF
 # Alle matcher blir puttet i lister
 func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner, kanskje.
-	
 	if len(text) < 9:
 		return false
 	for x in range(0, 3):
@@ -61,8 +56,8 @@ func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner
 	if text[6] != '-':
 		return false
 	
-	if $Prefix.pressed: 
-		text = text.lstrip('123456790-.') # Fjernet 1900 prefix
+	if $MC/VBC/Prefix.pressed: # Fjernet 1900 prefix
+		text = text.lstrip('123456790-.') 
 		if text.begins_with('L') and not TAGL.has(text):
 			TAGL.append(text)
 		if text.begins_with('A') and not TAGA.has(text):
@@ -71,7 +66,7 @@ func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner
 			TAGD.append(text)
 		if text.begins_with('R') and not TAGR.has(text):
 			TAGR.append(text)
-	else:
+	else: # Lagrer TAG med 1900 prefix
 		if text[5] == 'L' and not TAGL.has(text):
 			TAGL.append(text)
 		if text[5] == 'A' and not TAGA.has(text):
@@ -80,40 +75,51 @@ func is_tag(text): # TODO: Overkomplisert? Dele opp i flere sorteringsfunksjoner
 			TAGD.append(text)
 		if text[5] == 'R' and not TAGR.has(text):
 			TAGR.append(text)
+	
+	TAG = TAGL + TAGA + TAGD + TAGR # Lager en felles liste med alle TAG
 
 	return true # TODO: Return text i stedet?
 
 
+# Fyller scrollboxen med tagene som er funnet
+func fill_scrollbox():
+	for child in $MC/VBC/TagListe/VBC.get_children():
+		child.queue_free()
+	for tag in TAG:
+		var a = TAGlabel.instance()
+		a.text = tag
+		$MC/VBC/TagListe/VBC.add_child(a)
+
+
 # Funksjon for å lagre tag i .txt filer
 func save(text, id):
-	
 	if len(text) > 0:
 		var tag = PoolStringArray(text)
 		var tagsort = tag.join('\n')
 		var save = File.new()
-		save.open('C:\\Appl\\tagListe' + id + '.txt', File.WRITE)
+		save.open(dir + '\\tagListe' + id + '.txt', File.WRITE)
 		save.store_line(tagsort)
 		save.close()
 
 
 # Knapp for generering av lister
 func _on_GenererLister_pressed():
+	if $MC/VBC/Multifile.pressed: # Lagrer separate lister
+		save(TAGL, 'JSL')
+		save(TAGA, 'JSA')
+		save(TAGD, 'JSD')
+		save(TAGR, 'JSR')
+	if not $MC/VBC/Multifile.pressed: # Lagrer felles lister
+		save(TAG, 'JSF')
 	
-	save(TAGL, 'JSL')
-	save(TAGA, 'JSA')
-	save(TAGD, 'JSD')
-	save(TAGR, 'JSR')
-	save(TAG, 'JSF')
+	$MC/VBC/Label.text = 'Lister opprettet i ' + dir
 	
-	$Label.text = 'Lister opprettet i C:\\APPL'
-	
-	if $Taghub.pressed: # Åpner TagHub
+	if $MC/VBC/Taghub.pressed: # Åpner TagHub
 		OS.shell_open('https://taghub.equinor.com/')
 
 
 # Knapp for last/søk på nytt
 func _on_Reload_pressed():
-	
 	TAGL = []
 	TAGA = []
 	TAGD = []
@@ -125,4 +131,4 @@ func _on_Reload_pressed():
 
 # TODO: lage funksjon for å velge hvor filene skal opprettes
 func new_dir(path):
-	var dir = Directory.new()
+	dir = Directory.new()
